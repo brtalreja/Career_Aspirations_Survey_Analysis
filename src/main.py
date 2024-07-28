fa8402d8f1c77dce9ac581b625ef4e875fb7a5b1
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-07-28 16:52:19 -0500

    Long Term Employment Analysis

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ country = data["Your Current Country."].value_counts()
 label = country.index
 counts = country.values
 
-colors = ['gold', 'lightgreen']
+colors = ['Yellow', 'lime']
 
 fig = go.Figure(data=[go.Pie(labels = label, values = counts)])
 fig.update_layout(title_text = 'Demographics: Current Country')
@@ -62,7 +62,7 @@ fig.show()
 # Approximately 58% of the respondents choose what their parents want them to pursue or what their social idols do.
 # Remaining 42% of GenZ are influenced by successful influencers, social media like LinkedIn, and known associations.
 
-#Q2: Interest in Higher Education:
+#Q2: Interest in pursuing self-sponsored Higher Education:
 
 question2 = 'Would you definitely pursue a Higher Education / Post Graduation outside of India ? If only you have to self sponsor it.'
 question2 = data[question2].value_counts()
@@ -71,7 +71,7 @@ label = question2.index
 counts = question2.values
 
 fig = go.Figure(data=[go.Pie(labels = label, values = counts)])
-fig.update_layout(title_text = 'Q2: Interest in Higher Education (self sponsorship)')
+fig.update_layout(title_text = 'Q2: Interest in pursuing self-sponsored Higher Education')
 fig.update_traces(hoverinfo = 'label+value', textinfo = 'percent', textfont_size = 30,
                   marker = dict(colors = colors, line = dict(color = 'black', width = 3)))
 
@@ -81,3 +81,23 @@ fig.show()
 # 27.7% said no to pursuing higher education outside India.
 # While the remaining respondents said a conditional yes, i.e. they would not pursue it with their own expenses but if they get some scholarship or financial assistance, they will do it.
 
+#Q3: Interested in working for an employer for 3 years or more:
+
+question3 = 'How likely is that you will work for one employer for 3 years or more ?'
+question3 = data[question3].value_counts()
+
+label = question3.index
+counts = question3.values
+
+fig = go.Figure(data=[go.Pie(labels = label, values = counts)])
+fig.update_layout(title_text = 'Q3: Interested in working for an employer for 3 years or more')
+fig.update_traces(hoverinfo = 'label+value', textinfo = 'percent', textfont_size = 30,
+                  marker = dict(colors = colors, line = dict(color = 'black', width = 3)))
+
+fig.show()
+
+#COMMENT: 59.1% find it difficult to do so, but if the company is good, they might do it.
+# 33.6% are willing to do it without any hesitations.
+# Finally, 7.2% will not do it under any circumstances.
+
+#Q4: 
